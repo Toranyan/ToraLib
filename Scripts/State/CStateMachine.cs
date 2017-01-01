07f2e9d355116a2cb9428d7868b198c182fb344b
Manuel Traboco <toranyan86@gmail.com>
2017-01-01 17:55:16 +0900

    State Machine initial commit

---
+++ Scripts/State/CStateMachine.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CStateMachine {
+
+	//Properties
+	public IState CurrentState { 
+		get { return m_currentState; }
+		protected set { m_currentState = value; }
+	}
+
+	public IState PrevState {
+		get { return m_prevState; }
+		protected set { m_prevState = value; }
+	}
+
+	protected IState m_currentState;
+	protected IState m_prevState;
+
+	public void Update() {
+		if (CurrentState != null) {
+			CurrentState.Update ();
+		}
+	}
+
+	public void SetState(IState state) {
+		//exit state
+		if (CurrentState != null) {
+			CurrentState.OnExit (state);
+		}
+
+		//change
+		PrevState = CurrentState;
+		CurrentState = state;
+
+		//enter new state
+		if (CurrentState != null) {
+			CurrentState.OnEnter (PrevState);
+		}
+	}
+
+}
---
+++ Scripts/State/IState.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface IState {
+
+	void OnEnter (IState prevState);
+	void OnExit (IState nextState);
+	void Update ();
+
+}
