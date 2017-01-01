1cc4413382d78dd9f1a86d8dd21c873c5f088378
Manuel Traboco <toranyan86@gmail.com>
2017-01-01 17:57:30 +0900

    Unit class initial commit

---
+++ Scripts/Unit/CUnit.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace tora.unit {
+
+	public class CUnit {
+
+		public enum eState {
+			NONE,
+			MOVING,
+		};
+
+	}
+
+}
---
+++ Scripts/Unit/CUnitController.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using System.Collections.Generic;
+
+namespace tora.unit {
+
+	[RequireComponent(typeof(Rigidbody))]
+	public class CUnitController : MonoBehaviour {
+
+		//Serialized Fields
+
+		//unit params
+
+		[SerializeField]
+		protected float m_speed = 10;
+
+		[SerializeField]
+		public float m_moveThreshold = 1;
+
+		//Protected fields
+
+		//Rigidbody cache
+		protected Rigidbody m_rigidbody;
+		//
+		[SerializeField]
+		public Vector3 m_moveTarget;
+
+		[SerializeField]
+		public Vector3 m_vecMove;
+
+
+		//Properties
+
+		public Rigidbody Rigidbody {
+			get {
+				if (m_rigidbody == null) {
+					m_rigidbody = GetComponent<Rigidbody> ();
+				}
+				return m_rigidbody;
+			}
+		}
+
+
+		//Public Functions
+
+
+		/// <summary>
+		/// Send a move command to the unit to move to target location
+		/// </summary>
+		/// <param name="vecTarget">Vec target.</param>
+		public void CommandMove(Vector3 vecTarget) {
+			m_moveTarget = vecTarget;
+		}
+
+		//Protected Functions
+
+		protected void Update() {
+			//check commands
+			ProcessCommands();
+
+			//update AI
+
+			//update model
+
+		}
+
+		protected void ProcessCommands() {
+
+			if (Vector3.Distance (m_moveTarget, transform.position) > m_moveThreshold) {
+				m_vecMove = m_moveTarget - transform.position;
+			}
+
+
+
+		}
+
+
+		protected void UpdateAI() {
+
+		}
+
+		protected void UpdateModel() {
+
+		}
+
+		protected void FixedUpdate() {
+			//calc final velocity
+			Vector3 vecFinal = m_vecMove * m_speed;
+			this.Rigidbody.velocity = vecFinal;
+		}
+
+	}
+
+
+}
